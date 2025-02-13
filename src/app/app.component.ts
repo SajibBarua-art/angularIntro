8f6ee7e74af58be63e9b6922daba34c8b2745860
SajibBarua-art <sajibbaruanstu@gmail.com>
2025-02-13 22:36:37 +0600

    parent to child data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularIntro';
+  parentMessage: string = 'message coming from the parent component';
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  title: string = "List of posts";
+  messagePost: string = "List of posts";
 
+  postParentMessage: string = 'Message coming from the post parent';
+
+  @Input() fromParent!:string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  
 }
